effa0b59207d5155ef8ee68d4267cf3c053a59d6
samantathamiz <thamizsamanta@gmail.com>
2026-03-12 15:41:20 -0300

    Add examples of assignment operators in Python
    Demonstrate various assignment operators with example values.

--- 06-Operadores/exercicio_6-1_atribuicao.py
+++ 06-Operadores/exercicio_6-1_atribuicao.py
@@ -1,1 +1,24 @@
-
+a = 10; b = 15
+a = b                             #iguala a a b então b=15, a se torna 15 também
+print(a)
+a = 10; b = 15                    #retorna aos valores iniciais, senão serão sempre substituídos por valores da última expressão
+a += b #
+print (a)
+a = 10; b = 15
+a -= b
+print (a)
+a = 10; b = 15
+a *= b
+print(a)
+a = 10; b = 15
+a /= b
+print(a)
+a = 10; b = 15
+a //= b
+print(a)
+a = 10; b = 15
+a %= b
+print(a)
+a = 10; b = 15
+a **= b
+print(a)
