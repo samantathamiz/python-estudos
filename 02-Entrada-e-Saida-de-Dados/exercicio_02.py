66278b26622f3ee1081caf237cb3b53d5e44f181
samantathamiz <thamizsamanta@gmail.com>
2026-03-12 15:07:23 -0300

    Update exercicio_02.py

--- 02-Entrada-e-Saida-de-Dados/exercicio_02.py
+++ 02-Entrada-e-Saida-de-Dados/exercicio_02.py
@@ -1,1 +1,7 @@
-
+A = 'Uma frase'        #Variável A
+B = 'separada'         #Variável B
+x = 123                #Variável x
+print (A, B, x)        #imprime as variáveis 
+print (A, B, x, sep='-')      #imprime com separador
+print (A, B, x, end='nada')        #imprime com end = nada
+print (A, B, x, sep='-----')    #imprime com separador de 5 traços
