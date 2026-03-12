40027ea382a4370949369d29607b4babf99af0d6
samantathamiz <thamizsamanta@gmail.com>
2026-03-12 16:48:39 -0300

    Implement function to calculate double of a number
    Added a function to calculate double of a number.

--- 10-Funcoes/exercicio_10.py
+++ 10-Funcoes/exercicio_10.py
@@ -1,1 +1,4 @@
-
+def dobro(n):                 #def = define uma função
+    return n*2                # dobro é o nome da função
+print(dobro(5))               # n é o parâmetro da função, ou seja, o valor que a função irá receber 
+                              #nesse momento n=5, portanto dobro de 5 = 2 pois n*2 é o que o programa retorna
