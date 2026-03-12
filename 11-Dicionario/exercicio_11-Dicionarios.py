2a21f44f66173c339ebbc1be793267ae5beb6f1b
samantathamiz <thamizsamanta@gmail.com>
2026-03-12 16:58:33 -0300

    Update exercicio_11-Dicionarios.py

--- 11-Dicionario/exercicio_11-Dicionarios.py
+++ 11-Dicionario/exercicio_11-Dicionarios.py
@@ -1,1 +1,16 @@
+pessoa = {                                         #cria variável pessoa para guardar informações
+    "nome":"Ana",                                  #chaves { } significam que estou criando um dicionário 
+    "idade":"25"    
+}
+for chave,valor in pessoa.items():                 #chave e valor são etiquetas
+    print(chave, valor)                            #chaves => nome e idade são chaves
+                                                   #Ana e 25 são valores
 
+                                                   # FOR está percorrendo o dicionário
+                                                   # FOR = loop (repetição)
+                                                   # chave, valor => a cada volta do loop, me dê a chave e o valor
+                                                   # in significa dentro de.
+                                                   # .items () pega cada par chave + valor do dicionário
+                                                   # ou seja:
+                                                   # ("nome","Ana")
+                                                   # ("idade","25")    
