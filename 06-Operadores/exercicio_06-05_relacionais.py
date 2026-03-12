17dda8b8afd7fbe29709235d1b6ca289c42f720f
samantathamiz <thamizsamanta@gmail.com>
2026-03-12 16:06:56 -0300

    Update exercicio_06-05_relacionais.py

--- 06-Operadores/exercicio_06-05_relacionais.py
+++ 06-Operadores/exercicio_06-05_relacionais.py
@@ -1,1 +1,8 @@
-
+a = 4; b=20
+print (a == b)                               #compara usando == igual a
+print (a != b)                               #compara usando != diferente de
+print (a > b)                                #compara > maior que 
+print (a < b)                                #compara < menor que
+print (a >= b)                               #compara usando >= maior ou igual a
+print (a <= b)                               #compara usando <= menor ou igual a
+print ('Acabamos de relacionar', {a,b})
