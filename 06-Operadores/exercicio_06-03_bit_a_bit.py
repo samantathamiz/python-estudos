3d0ddd2aa995c802419b12ca0f0a6fbcaaa482e3
samantathamiz <thamizsamanta@gmail.com>
2026-03-12 15:49:57 -0300

    Add bitwise operations examples in Python

--- 06-Operadores/exercicio_06-03_bit_a_bit.py
+++ 06-Operadores/exercicio_06-03_bit_a_bit.py
@@ -1,1 +1,10 @@
-
+a = 1 & 2                           #explicações desses exerícios estão no caderno de estudos.
+print(a) 
+b = 2^1 
+print(b)
+c = (~2 & 1) | (2 & ~1)
+print(c)
+d = 8>>1
+print(d)
+e = 2<<3
+print(d)
